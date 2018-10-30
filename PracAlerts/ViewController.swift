f269a412a5d55a033b7f13dc1fba390d6708ad66
XCode <xcode@sala6pc24.cic>
2018-10-30 18:04:30 +0100

    Initial Commit

---
+++ PracAlerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracAlerts
+//
+//  Created by XCode on 30/10/18.
+//  Copyright © 2018 XCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
